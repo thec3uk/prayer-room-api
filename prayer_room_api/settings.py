301fd93efa724dc14dfac4554bfa7d8f9ec33447
Andrew Miller <info@akmiller.co.uk>
2025-02-06 11:36:29 +0000

    csrf errors

--- prayer_room_api/settings.py
+++ prayer_room_api/settings.py
@@ -161,6 +161,8 @@ class ProdSettings(Settings):
         "api.prayer.thec3.uk"
     ] + [f'172.17.0.{num}' for num in range(2,255)]
 
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTOCOL", "https")
+
 # The `use` method will find the right sub-class of ``BaseSettings`` to use
 # Based on the value of the `DJANGO_MODE` env var.
 __getattr__, __dir__ = Settings.use()
