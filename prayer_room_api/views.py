f3fb697ce0d620b64e07b3f4470893c330819b78
Andrew Miller <info@akmiller.co.uk>
2025-02-06 12:47:44 +0000

    OPTIONS not protected

--- prayer_room_api/views.py
+++ prayer_room_api/views.py
@@ -40,6 +40,11 @@ class PrayerPraiseRequestViewSet(ModelViewSet):
             return []
         return super().get_authenticators()
 
+    def get_permissions(self):
+        if self.request.method == 'OPTIONS':
+            return []
+        return super().get_permissions()
+
     def get_queryset(self):
         qst = super().get_queryset()
         location = self.request.query_params.get('location')
