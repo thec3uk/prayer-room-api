9bc59b8efdfc444f9a5346f7ed6a7224099fb613
Andrew Miller <info@akmiller.co.uk>
2025-04-05 20:43:44 +0100

    remove DisallowedHost logging

--- prayer_room_api/settings.py
+++ prayer_room_api/settings.py
@@ -22,27 +22,25 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ROOT_URLCONF = 'prayer_room_api.urls'
+ROOT_URLCONF = "prayer_room_api.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            str(BASE_DIR / 'prayer_room_api' / "templates")
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [str(BASE_DIR / "prayer_room_api" / "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'prayer_room_api.wsgi.application'
+WSGI_APPLICATION = "prayer_room_api.wsgi.application"
 
 
 # Password validation
@@ -50,16 +48,16 @@ WSGI_APPLICATION = 'prayer_room_api.wsgi.application'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -67,9 +65,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -78,23 +76,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-DJANGO_WEBHOOK = dict(MODELS=["prayer_room_api.PrayerPraiseRequest", "prayer_room_api.Setting"])
+DJANGO_WEBHOOK = dict(
+    MODELS=["prayer_room_api.PrayerPraiseRequest", "prayer_room_api.Setting"]
+)
 
 
 AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
-SENTRY_DSN = os.environ.get('SENTRY_DSN')
+SENTRY_DSN = os.environ.get("SENTRY_DSN")
 
 if SENTRY_DSN:
 
@@ -128,6 +128,11 @@ LOGGING = {
             "class": "logging.StreamHandler",
             "formatter": "verbose",
         },
+        "null": {
+            "level": "DEBUG",
+            "class": "logging.NullHandler",
+            "formatter": "verbose",
+        },
     },
     "root": {"level": "INFO", "handlers": ["console"]},
     "loggers": {
@@ -140,7 +145,7 @@ LOGGING = {
         "sentry_sdk": {"level": "ERROR", "handlers": ["console"], "propagate": False},
         "django.security.DisallowedHost": {
             "level": "ERROR",
-            "handlers": ["console"],
+            "handlers": ["null"],
             "propagate": False,
         },
     },
@@ -150,56 +155,76 @@ LOGGING = {
 class Settings(BaseSettings):
 
     # Allow production to override the secret key, but fall-back to something consistent.
-    SECRET_KEY = env('django-insecure-y&^-#s$ee58d8&&xi_9kl1^38x)!0-1hgby+ofhdl23$8d6)$u')
+    SECRET_KEY = env(
+        "django-insecure-y&^-#s$ee58d8&&xi_9kl1^38x)!0-1hgby+ofhdl23$8d6)$u"
+    )
 
     # DEBUG defaults to True, but can be overridden by env var `DJANGO_DEBUG`
-    DEBUG = env.bool(True, prefix='DJANGO_')
+    DEBUG = env.bool(True, prefix="DJANGO_")
 
     # Simple cases that don't need `self` can even use a lambda
-    MEDIA_ROOT = env(lambda self: BASE_DIR / 'media')
+    MEDIA_ROOT = env(lambda self: BASE_DIR / "media")
 
     # Methods will be transparently invoked by the __getattr__ implementation
     def INSTALLED_APPS(self):
-        return list(filter(None, [
-            'django.contrib.admin',
-            'django.contrib.auth',
-            'django.contrib.contenttypes',
-            'django.contrib.sessions',
-            'django.contrib.messages',
-            'django.contrib.staticfiles',
-            # Conditionally include an app
-            'debug_toolbar' if self.DEBUG else None,
-            'import_export',
-            'rest_framework',
-            'rest_framework.authtoken',
-            "corsheaders",
-            "django_webhook",
-            'allauth',
-            'allauth.account',
-            'allauth.headless',
-            'allauth.socialaccount',
-            'socialaccount.providers.churchsuite',
-            'prayer_room_api',
-        ]))
+        return list(
+            filter(
+                None,
+                [
+                    "django.contrib.admin",
+                    "django.contrib.auth",
+                    "django.contrib.contenttypes",
+                    "django.contrib.sessions",
+                    "django.contrib.messages",
+                    "django.contrib.staticfiles",
+                    # Conditionally include an app
+                    "debug_toolbar" if self.DEBUG else None,
+                    "import_export",
+                    "rest_framework",
+                    "rest_framework.authtoken",
+                    "corsheaders",
+                    "django_webhook",
+                    "allauth",
+                    "allauth.account",
+                    "allauth.headless",
+                    "allauth.socialaccount",
+                    "socialaccount.providers.churchsuite",
+                    "prayer_room_api",
+                ],
+            )
+        )
 
     def MIDDLEWARE(self):
-        return list(filter(None, [
-            'django.middleware.security.SecurityMiddleware',
-            'django.contrib.sessions.middleware.SessionMiddleware',
-            "corsheaders.middleware.CorsMiddleware",
-            'django.middleware.common.CommonMiddleware',
-            'django.middleware.csrf.CsrfViewMiddleware',
-            'django.contrib.auth.middleware.AuthenticationMiddleware',
-            'django.contrib.messages.middleware.MessageMiddleware',
-            'django.middleware.clickjacking.XFrameOptionsMiddleware',
-            "allauth.account.middleware.AccountMiddleware",
-            'whitenoise.middleware.WhiteNoiseMiddleware' if not self.DEBUG else False,
-            # Conditionally include a middleware
-            'debug_toolbar.middleware.DebugToolbarMiddleware' if self.DEBUG else False,
-        ]))
+        return list(
+            filter(
+                None,
+                [
+                    "django.middleware.security.SecurityMiddleware",
+                    "django.contrib.sessions.middleware.SessionMiddleware",
+                    "corsheaders.middleware.CorsMiddleware",
+                    "django.middleware.common.CommonMiddleware",
+                    "django.middleware.csrf.CsrfViewMiddleware",
+                    "django.contrib.auth.middleware.AuthenticationMiddleware",
+                    "django.contrib.messages.middleware.MessageMiddleware",
+                    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+                    "allauth.account.middleware.AccountMiddleware",
+                    (
+                        "whitenoise.middleware.WhiteNoiseMiddleware"
+                        if not self.DEBUG
+                        else False
+                    ),
+                    # Conditionally include a middleware
+                    (
+                        "debug_toolbar.middleware.DebugToolbarMiddleware"
+                        if self.DEBUG
+                        else False
+                    ),
+                ],
+            )
+        )
 
     # Parse the URL into a database config dict.
-    DEFAULT_DATABASE = env.dburl('sqlite:///db.sqlite3')
+    DEFAULT_DATABASE = env.dburl("sqlite:///db.sqlite3")
 
     CORS_ALLOWED_ORIGINS = [
         "http://localhost:4000",
@@ -207,23 +232,23 @@ class Settings(BaseSettings):
 
     ALLOWED_HOSTS = []
 
-    INTERNAL_IPS = ['localhost', '127.0.0.1']
+    INTERNAL_IPS = ["localhost", "127.0.0.1"]
 
     def DATABASES(self):
         return {
-            'default': self.DEFAULT_DATABASE,
+            "default": self.DEFAULT_DATABASE,
         }
-
 
     SOCIALACCOUNT_STORE_TOKENS = True
     ACCOUNT_EMAIL_REQUIRED = True
     SOCIALACCOUNT_EMAIL_AUTHENTICATION = True
-    ACCOUNT_LOGIN_METHODS = {'email'}
-    SOCIALACCOUNT_ADAPTER = 'prayer_room_api.adapters.SocialAccountAdapter'
+    ACCOUNT_LOGIN_METHODS = {"email"}
+    SOCIALACCOUNT_ADAPTER = "prayer_room_api.adapters.SocialAccountAdapter"
+
     def SOCIALACCOUNT_PROVIDERS(self):
 
-        CHURCHSUITE_CLIENT_ID = os.environ['CHURCHSUITE_CLIENT_ID']
-        CHURCHSUITE_CLIENT_SECRET = os.environ['CHURCHSUITE_CLIENT_SECRET']
+        CHURCHSUITE_CLIENT_ID = os.environ["CHURCHSUITE_CLIENT_ID"]
+        CHURCHSUITE_CLIENT_SECRET = os.environ["CHURCHSUITE_CLIENT_SECRET"]
         return {
             "churchsuite": {
                 "APP": {
@@ -234,9 +259,10 @@ class Settings(BaseSettings):
                     "key": "thec3",
                 }
             }
-       }
-       # SOCIALACCOUNT_PROVIDERS['churchsuite']['APP']['client_id'] = CHURCHSUITE_CLIENT_ID
-       # SOCIALACCOUNT_PROVIDERS['churchsuite']['APP']['secret'] = CHURCHSUITE_CLIENT_SECRET
+        }
+
+    # SOCIALACCOUNT_PROVIDERS['churchsuite']['APP']['client_id'] = CHURCHSUITE_CLIENT_ID
+    # SOCIALACCOUNT_PROVIDERS['churchsuite']['APP']['secret'] = CHURCHSUITE_CLIENT_SECRET
 
     def HEADLESS_FRONTEND_URLS(self):
         return {
@@ -262,16 +288,14 @@ class ProdSettings(Settings):
     STATIC_ROOT = env(env.Required)
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-    CORS_ALLOWED_ORIGINS = [
-        "https://prayer.thec3.uk"
+    CORS_ALLOWED_ORIGINS = ["https://prayer.thec3.uk"]
+
+    ALLOWED_HOSTS = ["api.prayer.thec3.uk"] + [
+        f"172.17.0.{num}" for num in range(2, 255)
     ]
 
-    ALLOWED_HOSTS = [
-        "api.prayer.thec3.uk"
-    ] + [f'172.17.0.{num}' for num in range(2,255)]
-
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-    CELERY_BROKER_URL = env(env.Required, key='RABBITMQ_URL')
+    CELERY_BROKER_URL = env(env.Required, key="RABBITMQ_URL")
 
     def HEADLESS_FRONTEND_URLS(self):
         return {
@@ -279,6 +303,7 @@ class ProdSettings(Settings):
             "account_reset_password_from_key": "https://app.org/account/password/reset/key/{key}",
             "account_signup": "https://app.org/account/signup",
         }
+
 
 # The `use` method will find the right sub-class of ``BaseSettings`` to use
 # Based on the value of the `DJANGO_MODE` env var.
